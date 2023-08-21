8cdfe6e57c9af39b85ddd7e989185fcd625e084d
On Fabian <onfabian@campus.tedchnion.ac.il>
2023-08-21 13:49:16 +0300

    Initial work on Inlining

--- project/src/profile.cpp
+++ project/src/profile.cpp
@@ -41,6 +41,57 @@ VOID ins_count(UINT64* counter, UINT32 c)
     *counter += c;
 }
 
+// A function to inline another function at the given trace
+VOID InlineFunction(TRACE trace, RTN targetRtn)
+{
+    // Get the address of the function to inline
+    ADDRINT targetAddr = RTN_Address(targetRtn);
+
+    // Iterate through all BBLs in the trace
+    for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)) {
+        // Get the list of instructions in the BBL
+        INS ins = BBL_InsHead(bbl);
+        while (INS_Valid(ins)) {
+            // Check if the instruction calls the target function
+            if (INS_IsCall(ins) && INS_DirectBranchOrCallTargetAddress(ins) == targetAddr) {
+                // Get the inlined instructions from the target function
+                for (INS targetIns = RTN_InsHead(targetRtn); INS_Valid(targetIns); targetIns = INS_Next(targetIns)) {
+                    // Clone the instruction and insert it in the current position
+                    INS newIns = INS_Clone(targetIns);
+                    BBL_InsertInstrumentCall(bbl, INS_Next(ins), AFUNPTR(INS_InsertDirectJump), IARG_INST_PTR, IARG_BRANCH_TARGET_ADDR, IARG_END);// IARG_RETURN_IP
+                }
+
+                // Remove the original call instruction
+                INS_Delete(ins);
+                break; // No need to process more instructions in this BBL
+            }
+            ins = INS_Next(ins);
+        }
+    }
+}
+
+
+/*
+VOID RtnCall(UINT64* counter)
+{
+    (*counter)++;
+}
+
+// Pin calls this function every time a new rtn is executed
+VOID Routine(RTN rtn, VOID *v)
+{
+    if (!RTN_IsDynamic(rtn))
+    {
+        return;
+    }
+    RTN_Open(rtn);
+ 
+    // Insert a call at the entry point of a routine to increment the call count
+    RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)RtnCall, IARG_RETURN_IP, RTN_Name(rtn).c_str(), IARG_END);
+ 
+    RTN_Close(rtn);
+}
+*/
 VOID trace(TRACE trace, VOID* v)
 {
     rtn_stat_t* rtn_stat;
@@ -68,9 +119,16 @@ VOID trace(TRACE trace, VOID* v)
     if (rtn_addr == TRACE_Address(trace)) {
         TRACE_InsertCall(trace, IPOINT_BEFORE, (AFUNPTR)rtn_count, IARG_PTR, &(rtn_stat->rtn_count), IARG_END);
     }
-    for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)) {
-        BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)ins_count, IARG_PTR, &(rtn_stat->ins_count), IARG_UINT32, BBL_NumIns(bbl), IARG_END);
-    }
+	
+    // Check if the current routine should be inlined
+    if (should_inline_routine(rtn)) {
+        // Perform function inlining for the current trace
+        InlineFunction(trace, rtn);
+    } else {
+        // Count instructions in the regular way
+        for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)) {
+            BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)ins_count, IARG_PTR, &(rtn_stat->ins_count), IARG_UINT32, BBL_NumIns(bbl), IARG_END);
+        }
 }
 
 VOID fini(INT32 code, VOID* v)
@@ -101,6 +159,8 @@ int collect_profile()
         return Usage();
     }
 */
+    // Register Routine to be called to instrument rtn
+    //RTN_AddInstrumentFunction(Routine, 0);
     TRACE_AddInstrumentFunction(trace, 0);
     PIN_AddFiniFunction(fini, 0);
     PIN_StartProgram();
